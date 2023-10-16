bedfa45f5d3e35d7fe5cdf5e0654d4a4e179a051
Barış <baris_kilinc@windowslive.com>
2023-10-16 17:01:05 +0300

    litte edit before commit for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Barış Kılınç",
+  title: "Web Designer & Rpa Developer",
+  email: "eklekmek07@gmail.com",
+  gitHub: "eklekmek07",
+  instagram: "b.rkop",
+  linkedIn: "barış-kılınç-3a4709183",
 };
 
 const primaryColor = "#4E567E";
